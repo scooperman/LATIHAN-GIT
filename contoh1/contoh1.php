0c7e5de55064940e76d929e6ffbeb3f75ab18a58
scooperman <86174082+scooperman@users.noreply.github.com>
2021-09-13 13:53:16 +0700

    latihan-git

---
+++ contoh1/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "perkenalkan saya rizki prayoga<br>";
+    echo "Saya mahasiswa kelas 19.3A.05";
+    }
+}
